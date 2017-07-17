04124705fcc01610020fce9a78b65111b8042391
TimsManter <timsmanter@gmail.com>
2017-07-17 13:31:23 +0200

    Add basic component

--- template/src/vue/App.vue
+++ template/src/vue/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app-container">
-
+    <hello message="outsider"></hello>
     <div id="main-wrapper">{{#router}}
       <md-layout md-gutter id="router-wrapper">
         <router-view></router-view>
@@ -13,6 +13,7 @@
   import Vue from "vue";{{#material}}
   import VueMaterial from "vue-material";{{/material}}
   import { Component, Watch, Prop } from "vue-ts-decorate";
+  import Hello from "./components/Hello.vue";
 
   @Component()
   export default class App extends Vue {
---
+++ template/src/vue/components/Hello.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Hello, {{ message }}!</h1>
+</template>
+
+<script lang="ts">
+  import Vue from "vue";{{#material}}
+  import VueMaterial from "vue-material";{{/material}}
+  import { Component, Watch, Prop } from "vue-ts-decorate";
+
+  export default class Hello extends Vue {
+    @Prop()
+    message: string;
+  }
+</script>
+
+<style lang="scss">
+
+</style>
